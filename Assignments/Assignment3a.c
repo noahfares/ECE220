c2687cc207e4462c470c80b2874a966417c60a27
Noah Fares <noahfares@gmail.com>
2022-04-21 23:45:27 -0600

    Second commit

--- Assignments/Assignment3a.c
+++ Assignments/Assignment3a.c
@@ -20,6 +20,7 @@ int main(void) {
     struct book *new;
     int i, len;
 
+    //*********************************************************
     current = (struct book *)malloc(sizeof(struct book));
     len = (int)strlen(book_title[0]);
     current->title = (char *) malloc((len+1) * sizeof(char));
@@ -27,20 +28,51 @@ int main(void) {
     len = (int)strlen(book_author[0]);
     current->author = (char *) malloc((len+1) * sizeof(char));
     strcpy(current->author, book_author[0]);
+    //*********************************************************
+
     current->next = NULL;
     current->previous = NULL;
     first = current;
 
     for (i=1; i<SIZE; i++) {
- // place your code here
+        if (i == 1) {
+        // set the values for the new node
 
+        len = (int)strlen(book_title[i+1]);
+        current->title = (char *) malloc((len+1) * sizeof(char));
+        strcpy(current->title, book_title[i+1]);
+        len = (int)strlen(book_author[i+1]);
+        current->author = (char *) malloc((len+1) * sizeof(char));
+        strcpy(current->author, book_author[i+1]); 
+
+        new = current;
+        current->next = new;
+        new->previous = current;
+        new->next = NULL;  
+
+        } else if (i == 2) {
+        // set the values for the current node
+        first->next = current;
+        current->previous = first;
+        current->next = new;
+
+        len = (int)strlen(book_title[i-1]);
+        current->title = (char *) malloc((len+1) * sizeof(char));
+        strcpy(current->title, book_title[i-1]);
+        len = (int)strlen(book_author[i-1]);
+        current->author = (char *) malloc((len+1) * sizeof(char));
+        strcpy(current->author, book_author[i-1]);
+
+        printf("%s", current->author);
+        printf("%s", new->author);
+        printf("%s", first->author);  
+
+        }
     }
 
-    current = first;
     i = 0;
     while (current != NULL) {
-// pleace your code here
-
+        
     }
 
     printf("END\n");
