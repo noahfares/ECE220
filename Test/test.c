53d098efc4d180edff82be0301feea8fa7f9106f
Noah Fares <noahfares@gmail.com>
2022-04-25 16:54:07 -0600

    Fourth commit

--- Test/test.c
+++ Test/test.c
@@ -1,12 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main(){
-    char name[20] = "Hello";
-    printf("%d", sizeof(name));
-    printf("%d", strlen(name));  
 
-    return 0;
-}
+struct engine
+{
+    int horse_power;
+    int engine_CC;
+};
+struct car
+{
+    char name[50];
+    int year;
+    struct engine motor;
+};
+struct car cars[3] = {"Honda Civic Type R", 2021, {306, 2000},
+
+"Toyota Supra", 2020, 255, 2000,
+"Ferrari Roma", 2022, {612, 8900}};
+struct car *my_car, *your_car, *our_car;
+my_car = cars;
+your_car = {my_car[2];
+our_car = cars + 1;
+printf("FIRST: my car: %s; and our car: %s\n",my_car->name, our_car->name);
+your_car = my_car;
+my_car = our_car + 1;
+printf("SECOND: my car: %s; and your car: %s\n",
+       my_car->name, your_car->name);
